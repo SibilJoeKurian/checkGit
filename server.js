3b850c36c7920bab77b01a4bfbfcfea9cee11ceb
SibilJoeKurian <sibil.kurian@gmail.com>
2021-03-04 21:47:27 -0500

    Update server.js

--- server.js
+++ server.js
@@ -4,3 +4,5 @@ console.log("Second Hello")
 
 console.log("I created a bug I am working on it even if bug is there I dont care")
 console.log("Bug fixed Happy friday!!!")
+
+console.log("I am creating another conflict in git hub file")
